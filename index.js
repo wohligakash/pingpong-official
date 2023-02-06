17fc25b19d4dd9a16a462a05e4060af9e5a801a9
wohligakash <akash.pawar@wohlig.com>
2023-02-06 16:50:56 +0530

    changes in home page response

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const result = calculateSum(2, 3, 5);
 console.log(result);
 
 app.get('/', (req, res) => {
-    res.send("Proper Prior Planning Prevents Poor Performance")
+    res.send("In case of fire - git commit > git push > RUNNNNNNN")
 })
 
 app.get('/ping', (req, res) => {
