81c6c57e63d04db4f23185e5cb47c462be368b37
wohligakash <akash.pawar@wohlig.com>
2023-02-08 10:25:00 +0530

    changes

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ const result = calculateSum(2, 3, 5);
 console.log(result);
 
 app.get('/', (req, res) => {
-  res.send("In case of fire - git commit > git push > RUNNNNNNN")
+    res.send("In case of fire - git commit > git push > RUNNNNNNN")
 });
 
 app.get('/ping', (req, res) => {
     res.json({ Response: "Pong" })
 });
 
-app.listen(8070, () => {console.log("Server is running on localhost:8070")});
+app.listen(8070, () => {
+    console.log("Server is running on localhost:8070");
+});
 
