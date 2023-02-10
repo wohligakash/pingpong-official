c6eb8e2e679c2a89643cbfc3bc8ef959c683fad1
wohligakash <akash.pawar@wohlig.com>
2023-02-10 10:32:14 +0530

    Changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const result = calculateSum(2, 3, 5);
 console.log(result);
 
 app.get("/", (req, res) => {
-    res.send("In case of fire - git commit > git push > RUNNNNN")
+    res.send("In case of fire - git commit > git push > RUNNNNNNNN")
 });
 
 app.get("/ping", (req, res) => {
