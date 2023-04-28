01ddcf04a70ecb8559cf49f4f04a76de995a223d
wohligakash <akash.pawar@wohlig.com>
2023-04-28 17:16:53 +0530

    check

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Add this to the VERY top of the first file loaded in your app
-var apm = require('elastic-apm-node').start({
+require('elastic-apm-node').start({
 
     // Override the service name from package.json
     // Allowed characters: a-z, A-Z, 0-9, -, _, and space
-    serviceName: '',
+    serviceName: 'newsshield',
     
     // Use if APM Server requires a secret token
-    secretToken: '',
+    secretToken: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IlNZX2tCVGIydEhxdU1EQTRYX2F0cHpSX05jMW5NUUhqYVpTQkI2bl8wZGcifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImFwbS1zZXJ2ZXItMTY4MjY1ODExMC1hcG0tc2VydmVyLXRva2VuLWY2bnc4Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFwbS1zZXJ2ZXItMTY4MjY1ODExMC1hcG0tc2VydmVyIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQudWlkIjoiODc2NjdjZmEtY2ZmNi00NDFjLWE1MTItMWUwZmExYTMwNWM0Iiwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50OmRlZmF1bHQ6YXBtLXNlcnZlci0xNjgyNjU4MTEwLWFwbS1zZXJ2ZXIifQ.vaRs6mVSnzKxOwAqwWlresC_Bt1_3_x--QiuM7F_sNzU9FhpDV_AS3vQx-76xPTKV56EISmuySKESAdgcKF81hsWT0KlhGFZqG4NVsm9AVG4SCgLP_vRUMF5FXH2u7r7kWpsnmFn42SkUUVslvmjKlBex41bfLPKHP4MDrGilFpN5fO6rxYKB7eRT9fvwim1LgfSiZ1koQuVwfpcaN1TYhLsZbyAaMfOr1xqun9vIf0Y9ogcy78mPOHrKbkl3IqLlkxm7TJl89VGiuhlZjY1n5M9bYQztC0wW17m2WnjR5GIZ6TL6U6KsqSG4dH6fL2FG4u_1p_P5sXKayw3kQIvYw',
     
     // Set the custom APM Server URL (default: http://localhost:8200)
     serverUrl: 'http://localhost:8200',
